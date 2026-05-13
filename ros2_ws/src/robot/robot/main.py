f141368f305626ef75a48ee3201372012e6e8a97
Scott Bloom <scottkbloom@gmail.com>
2026-05-12 18:08:06 -0700

    comments added for LIDAR tuning parameters for object avoidance

--- ros2_ws/src/robot/robot/main.py
+++ ros2_ws/src/robot/robot/main.py
@@ -80,6 +80,8 @@ def run(robot: Robot) -> None:
             #     (300.0, 2500.0),
             #     (1300.0, 2500.0),
             # ]
+            #==================
+            # Straight path for testing
             path_control_points = [
                 (0.0,   100),
                 (0, 200),
@@ -88,18 +90,29 @@ def run(robot: Robot) -> None:
             path = densify_polyline(path_control_points, spacing=400.0)
 
             robot._nav_follow_pp_path(
+                #Distance bot looks ahead on the path to detect objects
                 lookahead_distance=100.0,
+                #moving speed
                 max_linear_speed=140.0,
+                #angluar velocity that keeps rover along curved path
                 max_angular_speed=1.5,
+                #if bot is within this distance to the goal, it will stop and consider the goal reached
                 goal_tolerance=20.0,
+                #filters out obstacles further than this range
                 obstacles_range=450.0,
+                #angle of view for obstacle detection in front of the robot, centered around the forward direction
                 view_angle=math.radians(70.0),
+                #distance at which the robot considers an obstacle too close and will stop to avoid collision
                 safe_dist=250.0,
+                # Fixed contol loop number delay where further updates temporarily suspended. So it can commit to current avoidance manuever without changing its target trajectory
                 avoidance_delay=150,
+                # lookahead distance can be temporarily reduced by scaling factor to help with tracking newly generated path
                 alpha_Ld=0.7,
                 offset=270.0,
+                #bot considers a left and right lane, so total width = land_width*2
                 lane_width=500.0,
                 obstacle_avoidance=True,
+                #x distance of lane center from the origin (where robot starts)
                 x_L=300.0,
             )
             robot.planner.set_path(path)
